35dad5562f1406efea3074d796219df410b297d0
Jerry-Guo1020 <2593517388@qq.com>
2025-12-17 22:51:49 +0800

    feat: 添加pygame初始化代码和背景设置
    添加游戏初始化代码，包括屏幕设置、背景图片加载和音频系统初始化，为飞机大战游戏开发做准备

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("hello world")
+import pygame
+import sys
+# 更好的退出
+import traceback
+
+pygame.init()
+pygame.mixer.init()
+
+
+width = 400
+height = 700 
+
+bg_size = width, height
+screen = pygame.display.set_mode(bg_size)
+pygame.display.set_caption("飞机大战")
+
+background = pygame.image.load("images/background.png").convert()
+
+# 放入背景音乐和被攻击特效
+
