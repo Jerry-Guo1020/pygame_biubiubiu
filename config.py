a76764af5f2aa2007a472058c4265a322cb2f9ee
Jerry-Guo1020 <2593517388@qq.com>
2025-12-17 22:51:42 +0800

    feat(config): 添加游戏配置文件包含屏幕设置、飞机属性和子弹配置
    添加游戏基础配置文件，包含屏幕尺寸、背景颜色、敌机血量、飞机大小、爆炸效果、子弹类型和伤害值等游戏核心参数的配置。这些配置将用于初始化游戏环境和控制游戏行为。

---
+++ config.py
@@ -0,0 +1,45 @@
+# 屏幕设置
+SCREEN_WIDTH = 480
+SCREEN_HEIGHT = 852
+SCREEN_SIZE = (695, 852)
+BG_COLOR = (205, 205, 205)
+
+# 敌人飞机的血量属性设置
+HP_LIST = [1, 20, 100, 20]
+
+# 飞机大小
+PLANE_SIZE = [
+    {"width":51, "height":39 },
+    {"width":69, "height":89 },
+    {"width":165, "height":246 },
+    {"width":100, "height":124 },
+]
+
+# 爆炸效果计数
+PLANE_BOMB_TIME = [5, 8, 14, 8]
+
+# 最大的子弹数
+PLANE_MAXIUM_BULLET = [2, 5, 7, 8]
+
+# 子弹类型图片
+BULLET_TYPE_IMAGES = [
+    "bullet1.png",
+    "bullet-1.gif",
+    "bullet2.png",
+    "bullet.png",
+]
+
+# 子弹伤害
+BULLET_DAMAGE_VALUE = [1, 1, 3, 1]
+
+# 补给配置
+SUPPLY_IMAGES = []
+SUPPLT_SIZE = [
+    {"width":58, "height":88 },
+    {"width":60, "height":103 },
+]
+
+# 敌机数量限制
+ENEMY0_MAXIUM = 8
+ENEMY1_MAXIUM = 1
+ENEMY2_MAXIUM = 1
